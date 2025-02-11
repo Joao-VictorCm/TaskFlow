fd7057ad465d26f63d3ba431e5525d74315d1eaf
Joao-VictorCm <joaovictor27cm@gmail.com>
2025-02-11 20:23:56 -0300

    listando itens

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,16 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { Task } from './entities/task.entity';
 
 @Injectable()
 export class TasksService {
+  private tasks: Task[] = [
+    {
+      id: 1,
+      name: 'Concluir o curso de Nest',
+      description: 'Finalizar o curso entendendo todos os conceitos abordados',
+      completed: false,
+    },
+  ];
+
   findAll() {
-    return [{ id: 1, task: 'Comprar pão' }];
+    return this.tasks;
   }
 
   findOne(id: string) {
-    return 'Buscar tarefa com ID ' + id;
+    return this.tasks.find((task) => task.id === Number(id)); //verificando se tem o id mandado pelo usuario na lista de tarefas
   }
 
   create(body: any) {
-    return body;
+    const newId = this.tasks.length + 1; //criando um id novo
+
+    const newTask = {
+      id: newId,
+      ...body,
+    };
+
+    this.tasks.push(newTask); //.push para add a lista de tarefas
+
+    return newTask;
   }
 }
