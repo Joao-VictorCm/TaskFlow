f9667a2ef5331068fe0cd420cd460714ddac2a1b
Joao-VictorCm <joaovictor27cm@gmail.com>
2025-02-10 22:00:25 -0300

    criando modulos manualmente

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app/app.module';
+
+/*
+-src/app.module.ts: Módulo principal do app
+-src/app.controller.ts: Define as rotas e lidas com as requisições 
+-src/app.service.ts: contém a lógica de negócios, separado do controlador
+*/
 
 //arquivo que inicia o projeto
 async function bootstrap() {
