3bb462a5e92841962b0f405fa50c085df63beabb
Joao-VictorCm <joaovictor27cm@gmail.com>
2025-02-15 21:57:54 -0300

    Add respostas no Header com o Interceptors

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -15,12 +15,15 @@ import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
 import { LoggerInterceptor } from 'src/common/interceptors/logger.interceptor';
+import { BodyCreateTaskInterceptor } from 'src/common/interceptors/body-create-task.interceptor';
+import { AddHeaderIntercptor } from 'src/common/interceptors/add-hader.interceptor';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private readonly taskService: TasksService) {}
   @Get()
   @UseInterceptors(LoggerInterceptor)
+  @UseInterceptors(AddHeaderIntercptor)
   findAllTasks(@Query() paginationDto: PaginationDto) {
     return this.taskService.findAll(paginationDto);
   }
@@ -32,6 +35,7 @@ export class TasksController {
   }
 
   @Post('/create')
+  @UseInterceptors(BodyCreateTaskInterceptor)
   createTask(@Body() createTaskDto: CreateTaskDto) {
     console.log(createTaskDto);
 
