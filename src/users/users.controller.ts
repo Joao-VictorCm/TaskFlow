22c36f5a6be03a824ecac3be90e878242898dc71
Joao-VictorCm <joaovictor27cm@gmail.com>
2025-02-16 19:14:14 -0300

    Criado DTO para o Users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,13 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -8,4 +16,7 @@ export class UsersController {
   findOneUser(@Param('id', ParseIntPipe) id: number) {
     return this.userService.findOne(id);
   }
+
+  @Post()
+  createUser(@Body() createUserDto: CreateUserDto) {}
 }
