325a2bf0caf88cb96211c1df7dc96e5f24221711
Joao-VictorCm <joaovictor27cm@gmail.com>
2025-02-22 21:10:44 -0300

    Criando Guards

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -8,6 +8,7 @@ import {
   Patch,
   Post,
   Query,
+  UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
@@ -17,8 +18,10 @@ import { PaginationDto } from 'src/common/dto/pagination.dto';
 import { LoggerInterceptor } from 'src/common/interceptors/logger.interceptor';
 import { BodyCreateTaskInterceptor } from 'src/common/interceptors/body-create-task.interceptor';
 import { AddHeaderIntercptor } from 'src/common/interceptors/add-hader.interceptor';
+import { AuthAdminGuard } from 'src/common/guards/admin.guard';
 
 @Controller('tasks')
+@UseGuards(AuthAdminGuard)
 export class TasksController {
   constructor(private readonly taskService: TasksService) {}
   @Get()
