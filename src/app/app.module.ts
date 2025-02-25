589c3c8f6bcbd38bed87fac0dc63870cbf8dd2c2
Joao-VictorCm <joaovictor27cm@gmail.com>
2025-02-24 21:45:36 -0300

    Criando sistema de hash

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,11 @@ import { TasksModule } from 'src/tasks/tasks.module';
 import { UsersModule } from 'src/users/users.module';
 import { LoggerMiddleware } from 'src/common/middlewares/logger.middleware';
 import { APP_GUARD } from '@nestjs/core';
-import { AuthAdminGuard } from 'src/common/guards/admin.guard';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), TasksModule, UsersModule],
+  imports: [ConfigModule.forRoot(), TasksModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [
     AppService,
