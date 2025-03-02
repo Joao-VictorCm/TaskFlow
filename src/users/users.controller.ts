995165a3fbf4afd9d21d5917f81a16643c4efed6
Joao-VictorCm <joaovictor27cm@gmail.com>
2025-03-02 17:40:37 -0300

    Upload de varios arquivos de uma vez

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -18,7 +18,7 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { AuthTokenGuard } from 'src/auth/guards/auth-token.guard';
 import { TokenPatloadParm } from 'src/auth/param/token-payload.param';
 import { PayloadTokenDto } from 'src/auth/dto/payload-token.dto';
-import { FileInterceptor } from '@nestjs/platform-express';
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 import * as path from 'node:path';
 import * as fs from 'node:fs/promises';
 import { randomUUID } from 'node:crypto';
@@ -57,10 +57,12 @@ export class UsersController {
 
   @UseGuards(AuthTokenGuard)
   @UseInterceptors(FileInterceptor('file'))
+  //@UseInterceptors(FilesInterceptor('file'))
   @Post('upload')
   async uploadAvatar(
     @TokenPatloadParm() tokenPayLoad: PayloadTokenDto,
     @UploadedFile() file: Express.Multer.File,
+    //@UploadedFile() files: Express.Multer.File,
   ) {
     const mimeType = file.mimetype;
     const fileExtension = path
@@ -75,6 +77,19 @@ export class UsersController {
     const fileLocale = path.resolve(process.cwd(), 'files', fileName); //salvando a img na pasta files
 
     await fs.writeFile(fileLocale, file.buffer); //.buffer é o nome do arquivo da img
+
+    //Exemplo de codigo para varios uploads de um vez
+    // files.forEach(async (file) => {
+    //   const fileExtension = path
+    //     .extname(file.originalname)
+    //     .toLocaleLowerCase()
+    //     .substring(1);
+    //   const fileName = `${tokenPayLoad.sub}.${fileExtension}`;
+    //   const fileLocale = path.resolve(process.cwd(), 'files', fileName);
+
+    //   await fs.writeFile(fileLocale, file.buffer);
+    // });
+
     return true;
   }
 }
