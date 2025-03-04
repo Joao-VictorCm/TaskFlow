07941d5fcc4320116e7856400b6f46a9b9816bfa
Joao-VictorCm <joaovictor27cm@gmail.com>
2025-03-03 22:18:50 -0300

    Primeiro teste automatizado

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,5 +1,18 @@
-describe('UsersService', () => {
-  it('desveria testar o modulo users', () => {});
+/*Testes Unitarios
+> AAA
+> Configuração do test (Arrange)
+> Algo que deseja fazer a ação (Act)
+> Conferir se ação foi esperada (Assert)
+*/
 
-  test('testar se o users service foi definido', () => {});
+describe('UsersService', () => {
+  it('desveria testar o modulo users', () => {
+    const numero1 = 150;
+    const numero2 = 100;
+
+    const conta = numero1 - numero2;
+
+    expect(conta).toBe(50);
+    //espera que a conta seja igual a 50
+  });
 });
