41dec16354889a1a946521bd26724dcdfec7762c
Joao-VictorCm <joaovictor27cm@gmail.com>
2025-03-04 21:28:27 -0300

    Configurado o mock para o teste do userService

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -3,7 +3,8 @@
 > Configuração do test (Arrange)
 > Algo que deseja fazer a ação (Act)
 > Conferir se ação foi esperada (Assert)
-*/
+
+Exemplo de teste unitario 
 
 describe('UsersService', () => {
   it('desveria testar o modulo users', () => {
@@ -14,5 +15,42 @@ describe('UsersService', () => {
 
     expect(conta).toBe(50);
     //espera que a conta seja igual a 50
+  });
+});
+*/
+
+import { PrismaService } from 'src/prisma/prisma.service';
+import { UsersService } from './users.service';
+import { HashingServiceProtocol } from 'src/auth/hash/hash.service';
+import { Test, TestingModule } from '@nestjs/testing';
+
+describe('UsersService', () => {
+  let userService: UsersService;
+  let prismaService: PrismaService;
+  let hashingService: HashingServiceProtocol;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      //Isso é um mok ou seja tudo que tem na parte constructor  no userService
+      providers: [
+        UsersService,
+        {
+          provide: PrismaService,
+          useValue: {},
+        },
+        {
+          provide: HashingServiceProtocol,
+          useValue: {},
+        },
+      ],
+    }).compile();
+
+    userService = module.get<UsersService>(UsersService);
+    prismaService = module.get<PrismaService>(PrismaService);
+    hashingService = module.get<HashingServiceProtocol>(HashingServiceProtocol);
+  });
+  it('should be define users service', () => {
+    console.log(userService);
+    expect(userService).toBeDefined();
   });
 });
